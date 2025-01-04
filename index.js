3636270122f803ccd4a6ade7d75561ec3e9ccafa
newbie <newbiemill72@gmail.com>
2025-01-04 22:33:37 +0530

    sec

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { text } from "stream/consumers";
 import { ifError } from "assert";
 
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 
 const __filename = fileURLToPath(import.meta.url);
